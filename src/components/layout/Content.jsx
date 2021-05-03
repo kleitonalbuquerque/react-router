59ac813e248284ef68b540997dc2411d0f8a96da
kleitonalbuquerque <kleiton2102@gmail.com>
2021-05-03 02:20:28 -0300

    Navegação com parâmetro

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -2,14 +2,18 @@ import "./Content.css";
 import { Switch, Route } from "react-router-dom";
 
 import React from "react";
-import About from "../../views/examples/About";
 import Home from "../../views/examples/Home";
+import About from "../../views/examples/About";
+import Param from "../../views/examples/Param";
 
 const Content = (props) => (
   <div className="Content">
     <Switch>
       <Route path="/about">
         <About />
+      </Route>
+      <Route path="/param/:id">
+        <Param />
       </Route>
       <Route path="/">
         <Home />
--- src/components/layout/Menu.jsx
+++ src/components/layout/Menu.jsx
@@ -13,6 +13,12 @@ const Menu = (props) => (
         <li>
           <Link to="/about">Sobre</Link>
         </li>
+        <li>
+          <Link to="/param/1">Param #01</Link>
+        </li>
+        <li>
+          <Link to="/param/legal">Param #02</Link>
+        </li>
       </ul>
     </nav>
   </aside>
